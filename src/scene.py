ab217ee212bd3d2919c8dc6b177dfd31a7b4b5c8
Yelan Tao <yeltao@ethz.ch>
2022-05-03 19:23:48 +0200

    non graspable table

--- src/scene.py
+++ src/scene.py
@@ -5,7 +5,7 @@ from tdw.tdw_utils import TDWUtils
 from tdw.add_ons.oculus_touch import OculusTouch
 from tdw.vr_data.oculus_touch_button import OculusTouchButton
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from tdw.librarian import ModelLibrarian, ModelRecord
 from tdw.output_data import Bounds, OutputData, CameraMatrices
 from tdw.add_ons.third_person_camera import ThirdPersonCamera
@@ -42,14 +42,13 @@ class OculusTouchTestScene(Controller):
             function=self.end_trial,
         )
         self.add_ons.extend([self.vr])
-        path = EXAMPLE_CONTROLLER_OUTPUT_PATH.joinpath("image_capture")
-        print(f"Images will be save to: {path.resolve()}")
-        capture = ImageCapture(path=path, avatar_ids=["vr"], pass_masks=["_id"])
-        self.add_ons.append(capture)
+        self.path = EXAMPLE_CONTROLLER_OUTPUT_PATH.joinpath("image_capture")
+        self.capture = ImageCapture(path=self.path, avatar_ids=["vr"], pass_masks=["_img"])
+        self.add_ons.append(self.capture)
         self.communicate(
             [
                 TDWUtils.create_empty_room(12, 12),
-                {"$type": "set_target_framerate", "framerate": 30},
+                {"$type": "set_target_framerate", "framerate": 60},
             ]
         )
 
@@ -121,7 +120,7 @@ class OculusTouchTestScene(Controller):
                 {
                     "$type": "set_kinematic_state",
                     "id": table_id,
-                    "is_kinematic": False,
+                    "is_kinematic": True,  # kinematic object is non-graspable
                     "use_gravity": True,
                 },
                 {"$type": "set_mass", "mass": 50, "id": table_id},
@@ -189,6 +188,17 @@ class OculusTouchTestScene(Controller):
             )
 
         self.communicate(commands)
+
+        images = self.capture.images["vr"]
+        for i in range(images.get_num_passes()):
+            if images.get_pass_mask(i) == "_depth":
+                # Get the depth values.
+                depth_values = TDWUtils.get_depth_values(images.get_image(i), depth_pass="_depth", width=images.get_width(), height=images.get_height())
+                path = self.path.joinpath("depth")
+                num = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])
+                plt.imshow(depth_values)
+                plt.savefig("depth.png")
+                plt.show()
 
         # Wait until the trial is done.
         while not self.trial_done and not self.simulation_done:
