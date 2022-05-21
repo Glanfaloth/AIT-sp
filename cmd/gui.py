eef3ffba6a66b0b44d0eb21cafb30aabc64dd8aa
Glanfaloth <yeltao@student.ethz.ch>
2022-05-21 21:21:54 +0200

    add bathroom page

--- cmd/gui.py
+++ cmd/gui.py
@@ -1,3 +1,5 @@
+import random
+import os
 from tkinter import *
 from tkinter import font as tkfont
 class TDWApp(Tk):
@@ -51,6 +53,65 @@ class PageBathroom(Frame):
         button = Button(self, text="Go to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
+        # select a bathroom sink
+        SINK_NAMES = [
+            "sink_cabinet_unit_wood_beech_honey_porcelain_composite",
+            "sink_cabinet_unit_wood_oak_white_porcelain_composite",
+        ]
+
+        SINKS = [
+            ("random", "random"),
+            ("beech", "sink_cabinet_unit_wood_beech_honey_porcelain_composite"),
+            ("oak", "sink_cabinet_unit_wood_oak_white_porcelain_composite"),
+        ]
+
+        sink = StringVar()
+        sink.set("random")
+
+        sinkLabel = Label(self, text="Select a sink", fg="blue")
+        sinkLabel.pack()
+
+        for text, name in SINKS:
+            Radiobutton(self, text=text, variable=sink, value=name).pack(anchor=W)
+
+        # select a toothbrush
+        TOOTHBRUSH_NAMES = ["toothbrush", "b03_toothbrush", "generic_toothbrush_001"]
+        TOOTHBRUSHS = [
+            ("random", "random"),
+            ("toothbrush1", "toothbrush"),
+            ("toothbrush2", "b03_toothbrush"),
+            ("toothbrush3", "generic_toothbrush_001"),
+        ]
+        toothbrush = StringVar()
+        toothbrush.set("random")
+
+        toothbrushLabel = Label(self, text="Select a toothbrush", fg="blue")
+        toothbrushLabel.pack()
+
+        for text, name in TOOTHBRUSHS:
+            Radiobutton(self, text=text, variable=toothbrush, value=name).pack(anchor=W)
+
+        PASS_MASKS = [("img", "_img"), ("id", "_id"), ("depth", "_depth")]
+
+        # add a button to run the program
+        def click(sinkValue, toothbrushValue):
+            sinkArg = ""
+            if sinkValue == "random":
+                sinkValue = random.choice(SINK_NAMES)
+                sinkArg = " --sink " + sinkValue
+            elif sinkValue != "":
+                sinkArg = " --sink " + sinkValue
+            toothbrushArg = ""
+            if toothbrushValue == "random":
+                toothbrushValue = random.choice(TOOTHBRUSH_NAMES)
+                toothbrushArg = " --toothbrush " + toothbrushValue
+            elif toothbrushValue != "":
+                toothbrushArg = " --toothbrush " + toothbrushValue
+            os.system("python3 src\scene_bathroom.py"  + sinkArg + toothbrushArg)
+
+
+        btn = Button(self, text="Run", command=lambda: click(sink.get(), toothbrush.get()))
+        btn.pack()
 
 class PageOffice(Frame):
 
