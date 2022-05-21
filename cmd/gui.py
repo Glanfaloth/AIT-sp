b530554cb384c07ae3e609b3445b70ecb7c796e4
Yelan Tao <yeltao@ethz.ch>
2022-05-21 21:28:46 +0200

    one gui to rule them all

--- cmd/gui.py
+++ cmd/gui.py
@@ -2,6 +2,9 @@ import random
 import os
 from tkinter import *
 from tkinter import font as tkfont
+from tdw.librarian import ModelLibrarian
+librarian = ModelLibrarian()
+
 class TDWApp(Tk):
 
     def __init__(self, *args, **kwargs):
@@ -48,7 +51,7 @@ class PageBathroom(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
-        label = Label(self, text="This is page bathroom", font=controller.title_font)
+        label = Label(self, text="This is a bathroom", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         button = Button(self, text="Go to the start page",
                            command=lambda: controller.show_frame("StartPage"))
@@ -118,11 +121,100 @@ class PageOffice(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
-        label = Label(self, text="This is page office", font=controller.title_font)
+        label = Label(self, text="This is an office", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         button = Button(self, text="Go to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
+        # select a cup
+        cups = librarian.get_all_models_in_wnid("n03147509")  # cup
+        CUP_NAMES = [record.name for record in cups if not record.do_not_use]
+        CUP_NAMES.insert(0, "random")
+
+        cup = StringVar()
+        cup.set("random")
+
+        cupLabel = Label(self, text="Select a cup", fg="blue")
+        cupLabel.pack()
+
+        for name in CUP_NAMES:
+            Radiobutton(self, text=name, variable=cup, value=name).pack(anchor=W)
+
+        # select a fruit
+        FRUIT_NAMES = [
+            "apple",
+            "b03_banana_01_high",
+            "b04_banana",
+            "banana_fix2",
+            "orange",
+            "b04_orange_00",
+        ]
+
+        FRUITS = [
+            ("random", "random"),
+            ("apple", "apple"),
+            ("banana1", "b03_banana_01_high"),
+            ("banana2", "b04_banana"),
+            ("banana3", "banana_fix2"),
+            ("orange1", "orange"),
+            ("orange2", "b04_orange_00"),
+            ("none", "none"),
+        ]
+
+        fruit = StringVar()
+        fruit.set("random")
+
+        fruitLabel = Label(self, text="Select a fruit", fg="blue")
+        fruitLabel.pack()
+
+        for text, name in FRUITS:
+            Radiobutton(self, text=text, variable=fruit, value=name).pack(anchor=W)
+
+        # select a bread
+        BREAD_NAMES = ["bread", "b03_loafbread", "b03_burger"]
+        BREADS = [
+            ("random", "random"),
+            ("bread", "bread"),
+            ("loafbread", "b03_loafbread"),
+            ("burger", "b03_burger"),
+            ("none", "none"),
+        ]
+        bread = StringVar()
+        bread.set("random")
+
+        breadLabel = Label(self, text="Select a bread", fg="blue")
+        breadLabel.pack()
+
+        for text, name in BREADS:
+            Radiobutton(self, text=text, variable=bread, value=name).pack(anchor=W)
+
+        PASS_MASKS = [("img", "_img"), ("id", "_id"), ("depth", "_depth")]
+
+        # add a button to run the program
+        def click(cupValue, fruitValue, breadValue):
+            cupArg = ""
+            if cupValue == "random":
+                cupValue = random.choice(CUP_NAMES)
+                cupArg = " --cup " + cupValue
+            elif cupValue != "":
+                cupArg = " --cup " + cupValue
+            fruitArg = ""
+            if fruitValue == "random":
+                fruitValue = random.choice(FRUIT_NAMES)
+                fruitArg = " --fruit " + fruitValue
+            elif fruitValue != "":
+                fruitArg = " --fruit " + fruitValue
+            breadArg = ""
+            if breadValue == "random":
+                breadValue = random.choice(BREAD_NAMES)
+                breadArg = " --bread " + breadValue
+            elif breadValue != "":
+                breadArg = " --bread " + breadValue
+            os.system("python3 src\test.py" + cupArg + fruitArg + breadArg)
+
+
+        btn = Button(self, text="Run", command=lambda: click(cup.get(), fruit.get(), bread.get()))
+        btn.pack()
 
 if __name__ == "__main__":
     app = TDWApp()
